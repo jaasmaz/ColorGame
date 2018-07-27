b9feeba19516d6b426690ff5948b8fa0fe2d502f
jaasmaz <jaasmaz@yahoo.com>
2018-07-27 12:11:12 -0700

    add function to change all squares to the correct color and pick a random number

--- colorGame.js
+++ colorGame.js
@@ -8,8 +8,9 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
-var colorDisplay = document.getElementById("colorDisplay")
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,11 +24,26 @@ for(var i = 0; i < squares.length; i++){
         var clickedColor = this.style.backgroundColor;
         //compare color to pickedColor
         if(clickedColor === pickedColor){
-            console.log("Correct!");
+            messageDisplay.textContent = "Correct!"
+            changeColors(clickedColor);
         } else {
-            console.log("WRONG!!!");
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again"
         }
     });
-} 
+}
+
+function changeColors(color){
+    //loop through all squares
+    for(var i = 0; i < squares.length; i++){
+        //change each color to mach given color
+        squares[i].style.backgroundColor = color;
+    }
+
+}
 
 
+function pickColor(){
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
